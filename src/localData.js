4bc773e4abbfbb69460c148abfbcb279f3a5f811
Skyeler Yearsley <135404110+mrjagerr@users.noreply.github.com>
2023-11-05 16:34:32 -0600

    Addition of search page and local data

---
+++ src/localData.js
@@ -0,0 +1,78 @@
+export const Data = {
+    "kind": "books#volume",
+    "id": "s1gVAAAAYAAJ",
+    "etag": "86LEmUCoSuA",
+    "selfLink": "https://www.googleapis.com/books/v1/volumes/s1gVAAAAYAAJ",
+    "volumeInfo": {
+        "title": "Pride and Prejudice",
+        "authors": [
+            "Jane Austen"
+        ],
+        "publisher": "C. Scribner's sons",
+        "publishedDate": "1918",
+        "description": "Austen’s most celebrated novel tells the story of Elizabeth Bennet, a bright, lively young woman with four sisters, and a mother determined to marry them to wealthy men. At a party near the Bennets’ home in the English countryside, Elizabeth meets the wealthy, proud Fitzwilliam Darcy. Elizabeth initially finds Darcy haughty and intolerable, but circumstances continue to unite the pair. Mr. Darcy finds himself captivated by Elizabeth’s wit and candor, while her reservations about his character slowly vanish. The story is as much a social critique as it is a love story, and the prose crackles with Austen’s wry wit.",
+        "readingModes": {
+            "text": true,
+            "image": true
+        },
+        "pageCount": 401,
+        "printedPageCount": 448,
+        "dimensions": {
+            "height": "18.00 cm"
+        },
+        "printType": "BOOK",
+        "averageRating": 4,
+        "ratingsCount": 370,
+        "maturityRating": "NOT_MATURE",
+        "allowAnonLogging": false,
+        "contentVersion": "1.4.12.0.full.3",
+        "panelizationSummary": {
+            "containsEpubBubbles": false,
+            "containsImageBubbles": false
+        },
+        "imageLinks": {
+            "smallThumbnail": "http://books.google.com/books/content?id=s1gVAAAAYAAJ&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE712tLZcGA_0LhMN_s50PZGRgVRpCZkvONv-KTsSZRZUrAPm1NYVbBmxxct3p6XqUiIjlkDVp4LRtRvCEGPCMjV1_swQgHsYbRiCMt0cyVANOEsYEgulYdR10FDHfMYoG66gcz1h&source=gbs_api",
+            "thumbnail": "http://books.google.com/books/content?id=s1gVAAAAYAAJ&printsec=frontcover&img=1&zoom=1&edge=curl&imgtk=AFLRE72e-8TEe7uuIY4l3OkUxQYVK2LT2yUecxWCF2DvFEKGecnGCUQyIkvw0UOlS18VQw43w2e56Pjr914nRZeu1Lk4ZgtLQZsBS3WI2wvvMd-hv3IxrMzqy-HdNWWzA53e0nZ5otOn&source=gbs_api",
+            "small": "http://books.google.com/books/content?id=s1gVAAAAYAAJ&printsec=frontcover&img=1&zoom=2&edge=curl&imgtk=AFLRE72XyVMaPuiuYT0aV_NE4DvggeD4lSzptHOUHQEwDxmVlPoFLXnwb1cZmqbbZlnQpimw2dFoNkx0a9WFQ6LkkWdSr71DlJyUzF8ze9aeg644G2JWyqiknScyfXQjMVHVXZM9LL0l&source=gbs_api",
+            "medium": "http://books.google.com/books/content?id=s1gVAAAAYAAJ&printsec=frontcover&img=1&zoom=3&edge=curl&imgtk=AFLRE72XBMedJolcnI2NaAEGbEdYM4vEQYQal1qPL1iV1jLYbOR37HRe7SL8WrbyOIwa4FBH7wmbifUreGJ8QUyLQ-xDPCWpJJYFD6vSpYbFOfvv_-JadwY0LxPbjaZH6Ji3ZICSWtc0&source=gbs_api",
+            "large": "http://books.google.com/books/content?id=s1gVAAAAYAAJ&printsec=frontcover&img=1&zoom=4&edge=curl&imgtk=AFLRE70TA48BRtnuhIrRezDT6BnC6e4WsHokqBotRk_QDUDVmpUkqQjb8caUs0X75bM_vItveBoP1RBjNWnEptm84YYQi_gEraAmMj91zmMZDGJpHBUYI5w0uSOkdHFqJ4AvsBcXVAA8&source=gbs_api",
+            "extraLarge": "http://books.google.com/books/content?id=s1gVAAAAYAAJ&printsec=frontcover&img=1&zoom=6&edge=curl&imgtk=AFLRE70fsUrV5Ea0wVttgagetcy0-CD2fFK9JtJWa-pU9Uire0mSrF4PzCklqhNEDL68HTMVxosJ9AlQRCQTPYTasG0lRNf8Fiqt854LQoxx5Dre5SwIc1BQNVxWs3Im4BVkPGDugbIS&source=gbs_api"
+        },
+        "language": "en",
+        "previewLink": "http://books.google.com/books?id=s1gVAAAAYAAJ&hl=&source=gbs_api",
+        "infoLink": "https://play.google.com/store/books/details?id=s1gVAAAAYAAJ&source=gbs_api",
+        "canonicalVolumeLink": "https://play.google.com/store/books/details?id=s1gVAAAAYAAJ"
+    },
+    "layerInfo": {
+        "layers": [
+            {
+                "layerId": "geo",
+                "volumeAnnotationsVersion": "20"
+            }
+        ]
+    },
+    "saleInfo": {
+        "country": "US",
+        "saleability": "FREE",
+        "isEbook": true,
+        "buyLink": "https://play.google.com/store/books/details?id=s1gVAAAAYAAJ&rdid=book-s1gVAAAAYAAJ&rdot=1&source=gbs_api"
+    },
+    "accessInfo": {
+        "country": "US",
+        "viewability": "ALL_PAGES",
+        "embeddable": true,
+        "publicDomain": true,
+        "textToSpeechPermission": "ALLOWED",
+        "epub": {
+            "isAvailable": true,
+            "downloadLink": "http://books.google.com/books/download/Pride_and_Prejudice.epub?id=s1gVAAAAYAAJ&hl=&output=epub&source=gbs_api"
+        },
+        "pdf": {
+            "isAvailable": true,
+            "downloadLink": "http://books.google.com/books/download/Pride_and_Prejudice.pdf?id=s1gVAAAAYAAJ&hl=&output=pdf&sig=ACfU3U3dQw5JDWdbVgk2VRHyDjVMT4oIaA&source=gbs_api"
+        },
+        "webReaderLink": "http://play.google.com/books/reader?id=s1gVAAAAYAAJ&hl=&source=gbs_api",
+        "accessViewStatus": "FULL_PUBLIC_DOMAIN",
+        "quoteSharingAllowed": false
+    }
+}
--- src/pages/BooksDetailsPage/BooksDetailsPage.js
+++ src/pages/BooksDetailsPage/BooksDetailsPage.js
@@ -1,36 +1,36 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-
-
-
+import { Data } from "../../localData";
 
 const BooksDetailsPage = () => {
-    const [users,setUsers] = useState([])
+  const [users, setUsers] = useState(Data);
   useEffect(() => {
-fetchUsers();
+    // fetchUsers();
   }, []);
 
-
-    const fetchUsers = async () =>{
-        try{
-            let response= await axios.get(
-                "https://jsonplaceholder.typicode.com/users"
-            );
-            setUsers(response.data)
-        } catch (error){
-            console.log(error.message)
-        }
+  const fetchUsers = async () => {
+    try {
+      let response = await axios.get(
+        "https://www.googleapis.com/books/v1/volumes/s1gVAAAAYAAJ"
+      );
+      setUsers(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error.message);
     }
+  };
 
+  return (
+    <div>
+      <li>{users.volumeInfo.title}</li>
+      <li>
+        <img src={users.volumeInfo.imageLinks.small}></img>
+      </li>
 
-    return (  
-        <div>
-            test
-        {users && users.map(user =>{
-            return <li key = {user.id}> {user.name}<br/>{user.website} </li>
-        })}
-        </div>
-    );
-}
- 
+      <br></br>
+      <li>{users.volumeInfo.description}</li>
+    </div>
+  );
+};
+
 export default BooksDetailsPage;
