4d22eb336afad4680ce64c85caaf1669631d7fa9
Skyeler Yearsley <135404110+mrjagerr@users.noreply.github.com>
2023-11-05 20:16:44 -0600

    Update of search function

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,19 +2,21 @@
 import React, { useState } from "react";
 
 
-const SearchBar = (onSubmit) => {
+const SearchBar = (props) => {
  const[search,setSearch] =useState('');
 const handleSubmit =(event) =>{
   event.preventDefault();
-  onSubmit(search)
+
+  
 
 
 
 }
   return (
     <form onSubmit={handleSubmit}  className="searchbar">
-      <input value={search} onChange={(e) => setSearch(e.target.value)} type="text" id="search"></input>
-      <button type="submit"> Search</button>
+      <input value={search}  onChange={(e) => setSearch(e.target.value)} type="text" id="search"></input>
+      <button type="submit"  >{console.log(search)} search  </button>
+      
       
     </form>
   );
--- src/pages/BooksSearchpage/BooksSearchPage.js
+++ src/pages/BooksSearchpage/BooksSearchPage.js
@@ -9,29 +9,50 @@ import SearchBar from "../../components/SearchBar/SearchBar.jsx";
 
 
 
-
 const BooksSearchPage = () => {
     const [users, setUsers] = useState(Data);
+    const [books,setBooks] = useState([]);
+    
     useEffect(() => {
-    //  fetchUsers();
+    fetchUsers();
     }, []);
   
     const fetchUsers = async () => {
       try {
         let response = await axios.get(
-          "https://www.googleapis.com/books/v1/volumes?q=quilting"
+          `https://www.googleapis.com/books/v1/volumes?q=quilting`
         );
         setUsers(response.data);
-        console.log(response.data);
       } catch (error) {
         console.log(error.message);
       }
     };
     
+    
+      
+  async function getBooks(entry) {
+    let userinput = entry;
+    let fiilteredBooks= users.filter(function (users) {
+      if (users.volumeInfo.title.includes(userinput)) {
+        return true;
+      }
+      
+    });
+
+    setBooks(fiilteredBooks);
+    console.log(books);
+  }
+   
   
     return (
       <div>
-       <SearchBar onSubmit={fetchUsers}/>
+        test 
+    <SearchBar onSubmit ={getBooks} />
+    
+       
+        
+      
+       
       </div>
     );
   };
